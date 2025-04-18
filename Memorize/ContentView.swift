bb4907cf6804619db621e5cfdc106cd6bc3a6b52
mohamedobaya <mohamed.ali.obaya@gmail.com>
2025-04-18 09:52:16 +0200

    Finished Required Task 1 Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,41 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️", "👹"]
+    let emojis = ["👻", "🎃", "🕷️", "👹", "💀", "🕸️", "🧙🏻‍♀️", "🐈‍⬛", "👺", "😱", "☠️", "🍭"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self){ index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+        
+    }
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){ // LazyGrid uses as less space as it can
+            ForEach(0..<cardCount, id: \.self){ index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundStyle(.orange)
-        
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View{
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    var cardRemover: some View{
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
+
 struct CardView: View {
     let content: String
-    @State var isFaceUp = false // add @State to change variables inside a View as View is immutable
+    @State var isFaceUp = true // add @State to change variables inside a View as View is immutable
     
     var body: some View { // Views are immutable
         ZStack {
             let cardBase = RoundedRectangle(cornerRadius: 12) // local variable
-            if isFaceUp {
-                cardBase
-                    .fill(.white)
-                cardBase
-                    .strokeBorder(lineWidth: 2)
-                Text(content)
-                    .font(.largeTitle)
-            } else {
-                cardBase.fill()
+            Group {
+                cardBase.fill(.white)
+                cardBase.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
+            cardBase.fill().opacity(isFaceUp ? 0: 1)
         }.onTapGesture {
             isFaceUp.toggle()
         }
     }
 }
+
 #Preview {
     ContentView()
 }
